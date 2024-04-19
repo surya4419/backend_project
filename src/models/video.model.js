9fa424f3a185e153ecf8113b5db3a8772fa39c14
suryaa4419@gmail.com <surya4419@gmail.com>
2024-04-19 11:09:57 +0530

    added user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+import mongosse ,{Schema} from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type:String , //cloudinary url
+            required:true
+        },
+        thumbnails:{
+            type:String , //cloudinary url
+            required:true
+        },
+        title:{
+            type:String ,
+            required:true
+        },
+        description:{
+            type:String , 
+            required:true
+        },
+        duration:{
+            type:Number,
+            required:true
+
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+        owener:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+
+    },
+    {
+        timestamps:true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongosse.model("Video",videoSchema)
