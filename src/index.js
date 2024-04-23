12a22ed86c093233c491a58539b92c502c76f7c2
suryaa4419@gmail.com <surya4419@gmail.com>
2024-04-23 22:48:41 +0530

    added routes and controllers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
-import { mongoose } from "mongoose";
-import {DB_NAME} from "./constants.js"
 import connectDB from "./bd/index.js";
 
 dotenv.config({
     path:'./env'
-})
+});
 
-connectDB()  //helps to connect db
-.then(()=>{  //server starts working
-    application.listen(process.env.PORT ||8000, ()=>{
-        console.log(`Server is running at port: ${process.env.PORT}`);
-    })
+  
+connectDB()    //helps to connect db
+.then(()=>{     //helps to start server
+   app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running at port: ${process.env.PORT}`);
+   })
 })
 .catch((err)=>{
     console.log("MONGO db connection failed !!!", err);
