b25c431c22fbbc325f497d812b23bc77e297ceff
suryaa4419@gmail.com <surya4419@gmail.com>
2024-04-24 22:42:37 +0530

    register controller bugs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { app } from "./app.js";
 import connectDB from "./bd/index.js";
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 });
 
   
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,11 +15,12 @@ const uploadOnCloudinary = async(localFilePath)=>{
     try {
         if(!localFilePath) return null
         //upload the file on cloudinary
-        const resource = await cloudinary.uploader.upload(localFilePath,{
+        const response = await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
         //file has been uploaded on cloudinary
-        console.log("file is uploaded on cloudinary", response.url);
+        //console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath)  //remove the locally saved temporary file as the upload operation got failed
