39a8ff8928ecd6af8bebea83ca40cd9fe3a100a8
suryaa4419@gmail.com <surya4419@gmail.com>
2024-04-27 16:20:40 +0530

    completed register user

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,4 @@
 import {v2 as cloudinary} from "cloudinary"
-import { response } from "express";
 import fs from 'fs'
 
 
@@ -7,11 +6,12 @@ import fs from 'fs'
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key:process.env.CLOUDINARY_API_KEY,  
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
 
-const uploadOnCloudinary = async(localFilePath)=>{
+const uploadOnCloudinary = async (localFilePath)=>{
     try {
         if(!localFilePath) return null
         //upload the file on cloudinary
@@ -19,7 +19,7 @@ const uploadOnCloudinary = async(localFilePath)=>{
             resource_type:"auto"
         })
         //file has been uploaded on cloudinary
-        //console.log("file is uploaded on cloudinary", response.url);
+        console.log("file is uploaded on cloudinary", response.url);
         fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
